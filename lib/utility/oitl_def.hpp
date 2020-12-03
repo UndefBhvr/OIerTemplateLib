7c7d2a5d4486af056453935376cc66a247ba4f56
ILSYT <1756304680@qq.com>
2020-12-03 21:00:00 +0800

    Added header protection

--- lib/utility/oitl_def.hpp
+++ lib/utility/oitl_def.hpp
@@ -1,3 +1,6 @@
+#ifndef _OITL_UTILITY_OITL_DEF_HPP
+#define _OITL_UTILITY_OITL_DEF_HPP // C++ Header oitl_def.hpp
+
 #if defined(__GNUC__)
 	#define _OITL_LANG_VER __cplusplus
 	#if __GNUC__>=10
@@ -15,3 +18,5 @@
 #if __cpp_concepts>=_OITL_CONCEPT_LIMIT
 	#define _OITL_CONCEPT_AVAILABLE
 #endif
+
+#endif //C++ Header oitl_def.hpp
