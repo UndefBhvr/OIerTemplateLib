3e9a8d23afdb3a69cc33ba6d0d29e548cfb9f34d
c_cpp_a <111665869+c-cpp-a@users.noreply.github.com>
2023-04-19 20:06:11 +0800

    增加stable_sort
    接下来还会把实现放进detail

--- lib/algorithm/sort.hpp
+++ lib/algorithm/sort.hpp
@@ -5,38 +5,66 @@
 
 namespace oitl
 {
-
-template<typename _RandomAccessIter>
-void
-sort(  
+	
+	template<typename _RandomAccessIter>
+	void
+	sort(  
 		_RandomAccessIter FirstIter,
 		_RandomAccessIter LastIter
-	)
+		)
 {
 	__detail::_inner_sort(
 		FirstIter,
 		LastIter,
 		std::less<typename std::iterator_traits<_RandomAccessIter>::value_type>(),
 		std::__lg(LastIter-FirstIter)*2
-	);
+		);
 }
-
-template<typename _RandomAccessIter,typename _Cmp>
-void
-sort(  
+	
+	template<typename _RandomAccessIter,typename _Cmp>
+	void
+	sort(  
 		_RandomAccessIter FirstIter,
 		_RandomAccessIter LastIter,
 		_Cmp Compare
-	)
+		)
 {
 	__detail::_inner_sort(
 		FirstIter,
 		LastIter,
 		Compare,
 		std::__lg(LastIter-FirstIter)*2
-	);
+		);
 }
-
+	template<typename _RandomAccessIter>
+	void
+	stable_sort(
+		_RandomAccessIter FirstIter,
+		_RandomAccessIter LastIter
+		)
+{
+	__detail::less<typename _RandomAccessIter::value_type> cmp;
+	__detail::_inner_merge_sort(
+		FirstIter,
+		LastIter,
+		cmp
+		);
+}
+	template<typename _RandomAccessIter,typename _Cmp>
+	void
+	stable_sort(
+		_RandomAccessIter FirstIter,
+		_RandomAccessIter LastIter,
+		const _Cmp &cmp
+		)
+{
+	__detail::_inner_merge_sort(
+		FirstIter,
+		LastIter,
+		cmp
+		);
+}
+	
 } //namespace oitl
 
 #endif //C++ Header sort.hpp
