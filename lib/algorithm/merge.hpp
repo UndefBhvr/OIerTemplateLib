b4aba75f14c62bcdc058113db378960c744746d1
c_cpp_a <111665869+c-cpp-a@users.noreply.github.com>
2023-04-19 20:09:01 +0800

    更新merge
    这是为了避免和stable_sort中使用的less起冲突

--- lib/algorithm/merge.hpp
+++ lib/algorithm/merge.hpp
@@ -1,49 +1,59 @@
-#ifndef _OITL_ALGORITHM_MERGE_HPP
-#define _OITL_ALGORITHM_MERGE_HPP // C++ Header merge.hpp
+#ifndef _OITL_ALGORITHM_MERGE_BASE_HPP
+#define _OITL_ALGORITHM_MERGE_BASE_HPP // C++ Header merge_base.hpp
 
-#include "detail/merge_base.hpp"
+#include<functional>
 
-namespace oitl
-{
-	
-	template<typename _RandomAccessIter,typename _Cmp>
-	void
-	merge(
-		_RandomAccessIter BeginIt1,
-		_RandomAccessIter EndIt1,
-		_RandomAccessIter BeginIt2,
-		_RandomAccessIter EndIt2,
-		_RandomAccessIter SaveIt,
-		const _Cmp &cmp
-		){
-			typename _RandomAccessIter::difference_type distance1=EndIt1-BeginIt1,distance2=EndIt2-BeginIt2;
-			if(distance1==1){
-				oitl::__detail::inner_insert(BeginIt1,BeginIt2,EndIt2,SaveIt,cmp);
-			} else if(distance2==1){
-				oitl::__detail::inner_insert(BeginIt2,BeginIt1,EndIt1,SaveIt,cmp);
-			} else{
-				oitl::__detail::inner_merge(BeginIt1,EndIt1,BeginIt2,EndIt2,SaveIt,cmp);
+namespace oitl {
+	namespace __detail {
+#ifndef _DEFINED_LESS
+#define _DEFINED_LESS
+		template<typename _Tp>
+		struct less{
+			constexpr bool operator()(const _Tp &lhs,const _Tp &rhs) const{
+				return lhs<rhs;
+			}
+		};
+#endif
+		template<typename _RandomAccessIter,typename _Cmp>
+		void inner_merge(_RandomAccessIter BeginIt1,_RandomAccessIter EndIt1,_RandomAccessIter BeginIt2,_RandomAccessIter EndIt2,_RandomAccessIter SaveIt,const _Cmp &cmp){
+			while(BeginIt1!=EndIt1 && BeginIt2!=EndIt2){
+				if(cmp(*BeginIt1,*BeginIt2)){
+					*SaveIt=*BeginIt1;
+					++SaveIt,++BeginIt1;
+				} else{
+					*SaveIt=*BeginIt2;
+					++SaveIt,++BeginIt2;
+				}
+			}
+			while(BeginIt1!=EndIt1){
+				*SaveIt=*BeginIt1;
+				++SaveIt,++BeginIt1;
+			}
+			while(BeginIt2!=EndIt2){
+				*SaveIt=*BeginIt2;
+				++SaveIt,++BeginIt2;
 			}
 		}
-	template<typename _RandomAccessIter>
-	void
-	merge(
-		_RandomAccessIter BeginIt1,
-		_RandomAccessIter EndIt1,
-		_RandomAccessIter BeginIt2,
-		_RandomAccessIter EndIt2,
-		_RandomAccessIter SaveIt
-		){
-			typename _RandomAccessIter::difference_type distance1=EndIt1-BeginIt1,distance2=EndIt2-BeginIt2;
-			if(distance1==1){
-				oitl::__detail::inner_insert(BeginIt1,BeginIt2,EndIt2,SaveIt);
-			} else if(distance2==1){
-				oitl::__detail::inner_insert(BeginIt2,BeginIt1,EndIt1,SaveIt);
-			} else{
-				oitl::__detail::inner_merge(BeginIt1,EndIt1,BeginIt2,EndIt2,SaveIt);
-			}
+		template<typename _RandomAccessIter>
+		inline void inner_merge(_RandomAccessIter BeginIt1,_RandomAccessIter EndIt1,_RandomAccessIter BeginIt2,_RandomAccessIter EndIt2,_RandomAccessIter SaveIt){
+			__detail::less<typename _RandomAccessIter::value_type> cmp;
+			inner_merge(BeginIt1,EndIt1,BeginIt2,EndIt2,SaveIt,cmp);
 		}
-	
+		template<typename _RandomAccessIter,typename _Cmp>
+		void inner_insert(const _RandomAccessIter &BeginIt1,_RandomAccessIter BeginIt2,_RandomAccessIter EndIt2,_RandomAccessIter SaveIt,const _Cmp &cmp){
+			while(!cmp(*BeginIt2,*BeginIt1)){
+				if(BeginIt2==EndIt2)	break;
+				*(SaveIt++)=*(BeginIt2++);
+			}
+			*(SaveIt++)=*(BeginIt1);
+		}
+		template<typename _RandomAccessIter>
+		void inner_insert(const _RandomAccessIter &BeginIt1,_RandomAccessIter &BeginIt2,_RandomAccessIter &EndIt2,_RandomAccessIter &SaveIt){
+			__detail::less<typename _RandomAccessIter::value_type> cmp;
+			inner_insert(BeginIt1,BeginIt2,EndIt2,SaveIt,cmp);
+		}
+	}	
+
 } //namespace oitl
 
-#endif //C++ Header sort.hpp
+#endif //C++ Header merge_base.hpp
